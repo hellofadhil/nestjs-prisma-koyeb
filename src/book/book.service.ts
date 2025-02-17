66582bc60baa677790289d85ad3e850728172211
fadhil1806 <fadhil8637@smk.belajar.id>
2025-02-17 14:24:29 +0700

    first commit

---
+++ src/book/book.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class BookService {
+  constructor(private prisma: PrismaService) { }
+
+  create(createBookDto: CreateBookDto) {
+    return 'This action adds a new book';
+  }
+
+  async findAll() {
+    return this.prisma.books.findMany({
+      include: {
+        category: true,
+        author: true
+      }
+    })
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} book`;
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
